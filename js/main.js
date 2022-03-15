43ec6741b028cea3a3f3a3fd9fe32f33f73cdf18
Vitaliy901 <allanamos90@gmail.com>
2022-03-15 17:37:39 +0200

    error.

--- js/main.js
+++ js/main.js
@@ -6,39 +6,15 @@ let images = slider.querySelectorAll('img')
 let src = []
 let offset = 0;
 let step = 0;
-let flag = true;
 // copy src and remove all img===========
 for (let i = 0; i < images.length; i++) {
 	src[i] = images[i].src
 	images[i].remove()
 }
 //  fill slider ======================
-for (let i = 0; i < 6; i++) {
-	let img;
-	if (flag) {
-		img = document.createElement('img')
-		img.src = src[src.length - 1]
-		img.style.left = -480 + "px"
-		slider.appendChild(img)
-		flag = false
-		continue
-	}
-	if (step < src.length) {
-		img = document.createElement('img')
-		img.src = src[step]
-		img.style.left = offset * 480 + "px"
-		step++
-		offset++
-	} else {
-		img = document.createElement('img')
-		img.src = src[0]
-		img.style.left = offset * 480 + "px"
-		step = 0;
-		offset = 4
-	}
-	slider.appendChild(img)
+for (let i = 0; i < 5; i++) {
+	createImg()
 }
-
 //  create img and load into end ==========
 function createImg(params) {
 	let img = document.createElement('img')
@@ -58,12 +34,10 @@ function createImg(params) {
 
 function move(params) {
 	let imagesExist = slider.querySelectorAll('img')
-
 	let offset2 = 0;
 	if (this.matches('.right')) {
 		for (let img of imagesExist) {
 			img.style.left = `${offset2 * 480 - 480}px`
-			console.log(img.style.left)
 			offset2++
 		}
 		createImg()
@@ -73,7 +47,6 @@ function move(params) {
 	this.removeEventListener('click', move)
 	setTimeout(function (params) {
 		imagesExist[0].remove()
-		imagesExist[1].remove()
 		params.addEventListener('click', move)
 	}, 900, this)
 }
